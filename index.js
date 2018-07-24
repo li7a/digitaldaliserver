715b57e9a090e8189e9f084b151e6c2ee51c31dd
li7a <35306451+li7a@users.noreply.github.com>
2018-07-24 21:41:41 +0300

    finish

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 5000;
+
+app.use(express.static(__dirname + '/public'))
+
+app.get("*", (req, res) => { res.sendFile(__dirname + "/public/index.html") });
+
+app.listen(PORT, () => {
+  console.log(`PORT=${PORT}`);
+});
